9f848af64aec7eb1dea90b4171ff6840b78557d3
Artemiy Martyanov <artemiy@martyanov.xyz>
2020-12-10 12:49:43 +0000

    migrate to express

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+const express = require('express');
+const bodyParser = require('body-parser');
 const axios = require('axios');
-const credentials = require('./credentials.json');
 const admin = require("firebase-admin");
 
 const serviceAccount = require("./vk-messenger-flutter-firebase-adminsdk-pms5f-f56dc9b70c.json");
@@ -13,14 +14,64 @@ const vkTransport = axios.create({
   baseURL: "https://api.vk.com/method",
 });
 
+const app = express();
+const port = 3001;
+
+app.use(bodyParser.json());
+
+let interval;
 let processing = false;
+let vkToken;
+let phoneToken;
 let lastMessageId;
 
-const init = async () => {
+app.post('/', (req, res) => {
+  if (interval) {
+    clearInterval(interval);
+  }
+  vkToken = req.body.vkToken;
+  phoneToken = req.body.phoneToken;
+  console.log('new params!');
+  console.log(req.body);
+  processMessages();
+  setInterval(processMessages, 5000); 
+  res.send('ok');
+});
+
+app.listen(port, () => {
+  console.log(`VkPush app listening at http://localhost:${port}`)
+});
+
+const processMessages = async () => {
   try {
     if (processing) {
       return;
     }
+    await (async () => {
+      const { data: { response, error } } = 
+        await vkTransport.get(`/messages.getConversations?offset=0&count=1&access_token=${vkToken}&v=5.126`);
+      if (error) {
+        return;
+      }
+      const item = response && response.items.length && response.items[0];
+      if (item.last_message.id === lastMessageId) {
+        return;
+      }
+      if (item && item.conversation.unread_count) {
+        await admin.messaging().send({
+          token: phoneToken,
+          notification: {
+            title: "Новое сообщение",
+            body: item.last_message.text,
+          },
+          // Set Android priority to "high"
+          android: {
+            priority: "high",
+          },
+        });
+        lastMessageId = item.last_message.id;
+      }
+    })();
     processing = true;
     await processMessages();
   } catch (e) {
@@ -28,32 +79,3 @@ const init = async () => {
   }
   processing = false;
 };
-
-const processMessages = async () => {
-  const { data: { response, error } } = await vkTransport.get(`/messages.getConversations?offset=0&count=1&access_token=${credentials.vktoken}&v=5.126`);
-  if (error) {
-    return;
-  }
-  const item = response && response.items.length && response.items[0];
-  console.log(item.last_message.id, lastMessageId);
-  if (item.last_message.id === lastMessageId) {
-    return;
-  }
-  if (item && item.conversation.unread_count) {
-    await admin.messaging().send({
-      token: credentials.phonetoken,
-      notification: {
-        title: "Новое сообщение",
-        body: item.last_message.text,
-      },
-      // Set Android priority to "high"
-      android: {
-        priority: "high",
-      },
-    });
-    lastMessageId = item.last_message.id;
-  }
-}
-
-init();
-setInterval(init, 5000);
